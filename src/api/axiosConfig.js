83122984d41d44dfa88df2ab2f6a34c879647ade
Maheshkumar <maheshkumarjadhav2001@gmail.com>
2026-02-04 00:40:03 +0530

    baseURL changed for Deployment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+ // baseURL: "http://localhost:8080",
+  baseURL: "https://e-grampanchayat-a-digital-governance-production.up.railway.app",
+
 });
 
 api.interceptors.request.use((config) => {
